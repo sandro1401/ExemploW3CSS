1642b19e09aa57089274688b9ef7465028b8f8b1
sandro1401 <sandro.jteixeira39@gmail.com>
2024-06-06 08:32:05 -0300

    primeiro commit exerc pfs petShoop

---
+++ produtos.js
@@ -0,0 +1,13 @@
+window.onload = () => {
+    document.querySelector("#produtos").innerHTML = ` 
+        <div class="w3-col l4 m6 s12 w3-container w3-padding-16">
+            <div class="w3-card">
+                <div class="w3-container w3-center">
+                    <img src="pedigree.jpg" style="width: 70%">
+                    <h5>Informações do Produto - lorem ipsum dolor</h5>
+                    <h3 class="w3-blue">R$ 400,00</h3>
+                </div>
+            </div>
+        </div>
+    `
+}
